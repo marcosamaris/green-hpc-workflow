a7f6ddf832ab4ee9257a237b3488a0f3bc6aa0a3
Felipe Barbosa <rafaelt.ibarbosa@gmail.com>
2024-08-19 19:14:46 -0300

    Wfgen Done

--- src/wfgenrecipe.py
+++ src/wfgenrecipe.py
@@ -2,17 +2,26 @@ import pathlib
 from wfcommons.wfchef.recipes import MontageRecipe
 from wfcommons import WorkflowGenerator
 
-amount_recipe = int(input('Type of amount of recipe: '))
-
 try:
-    if amount_recipe > 59:
-        for amount_tasks in range(60, amount_recipe, 1):
-            generator = WorkflowGenerator(MontageRecipe.from_num_tasks(amount_tasks))
-            workflows = generator.build_workflows(1)
-            
-            for index, workflow in enumerate(workflows):
-                workflow.write_json(pathlib.Path(f'montage-workflow-{amount_tasks}-{index}.json'))
-    else:
-        print('Amount of recipes must be greater than 0!')
-finally:
-    print('DONE!')
+    amount_recipe = int(input('Type amount of recipe: '))
+except ValueError:
+    print('Please enter a valid integer.')
+    exit(1)
+
+
+MIN_TASKS = 60
+
+output_dir = pathlib.Path(__file__).parent.parent / 'data'
+output_dir.mkdir(parents=True, exist_ok=True)
+
+for amount_tasks in range(MIN_TASKS, amount_recipe + MIN_TASKS):
+    generator = WorkflowGenerator(MontageRecipe.from_num_tasks(amount_tasks))
+    workflows = generator.build_workflows(1)
+    
+    for index, workflow in enumerate(workflows):
+        output_path = output_dir / f'montage-workflow-{amount_tasks}-{index}.json'
+        try:
+            workflow.write_json(output_path)
+        except Exception as e:
+            print(f"Error writing {output_path}: {e}")
+
