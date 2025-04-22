07846b258b501f986a48c8981ab857efa9ec9e33
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2025-04-22 17:27:09 -0300

    new start.sh to choose between 2 workflow

--- src/wfmontagegenrecipe.py
+++ src/wfmontagegenrecipe.py
@@ -1,6 +1,5 @@
 import pathlib
-# from wfcommons.wfchef.recipes import MontageRecipe
-from wfcommons.wfchef.recipes import BlastRecipe
+from wfcommons.wfchef.recipes import MontageRecipe
 from wfcommons import WorkflowGenerator
 
 try:
@@ -9,20 +8,18 @@ except ValueError:
     print('Please enter a valid integer.')
     exit(1)
 
-
 MIN_TASKS = 60
 
-output_dir = pathlib.Path(__file__).parent.parent / 'workflows'
+output_dir = pathlib.Path(__file__).parent.parent / 'workflows' / 'montage'
 output_dir.mkdir(parents=True, exist_ok=True)
 
 for amount_tasks in range(MIN_TASKS, MIN_TASKS + amount_recipe * 10, 10):
-    generator = WorkflowGenerator(BlastRecipe.from_num_tasks(amount_tasks))
+    generator = WorkflowGenerator(MontageRecipe.from_num_tasks(amount_tasks))
     workflows = generator.build_workflows(1)
-    
+
     for index, workflow in enumerate(workflows):
-        output_path = output_dir / f'blast-workflow-{amount_tasks}-{index}.json'
+        output_path = output_dir / f'montage-workflow-{amount_tasks}-{index}.json'
         try:
             workflow.write_json(output_path)
         except Exception as e:
             print(f"Error writing {output_path}: {e}")
-
--- start.sh
+++ start.sh
@@ -2,15 +2,32 @@
 
 platform="platforms/apollo_2000_platform.xml"
 
-for recipe in workflows/* ; do
+echo "Escolha o workflow para executar:"
+echo "1 - blast (simula a execução de buscas em bases de dados biológicas.)"
+echo "2 - montage (realiza a montagem de mosaicos de imagens astronômicas.)"
+read -p "Digite 1 ou 2: " escolha
+
+case $escolha in
+    1)
+        workflow_dir="workflows/blast"
+        ;;
+    2)
+        workflow_dir="workflows/montage"
+        ;;
+    *)
+        echo "Opção inválida. Saindo..."
+        exit 1
+        ;;
+esac
+
+for recipe in "$workflow_dir"/* ; do
     my_recipe=$(basename "$recipe")
-    if [[ "$my_recipe" =~ ^blast-workflow-[0-9]+-[0-9]+\.json$ ]]; then
-        execute_recipe="workflows/$my_recipe"
-        $(build/my-wrench-simulator "$platform" "$execute_recipe" --wrench-energy-simulation)
+    if [[ "$my_recipe" =~ \.json$ ]]; then
+        execute_recipe="$workflow_dir/$my_recipe"
+        ./build/my-wrench-simulator "$platform" "$execute_recipe" --wrench-energy-simulation
     fi
 done
 
-echo "All recipes have been executed"
+echo "Todos os recipes foram executados."
 
 exit
-
